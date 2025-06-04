fdd5fd9f6fcec90f1f2940112b2b8de61aad742c
Jawad Tech <jawadtechxd@gmail.com>
2025-06-04 11:57:00 +0500

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -33,7 +33,7 @@ async (conn, mek, m, { from, reply }) => {
 
         // Send image with caption
         await conn.sendMessage(from, {
-            image: { url: `https://files.catbox.moe/juroe8.jpg` },
+            image: { url: `https://files.catbox.moe/7zfdcq.jpg` },
             caption: formattedInfo,
             contextInfo: { 
                 mentionedJid: [m.sender],
